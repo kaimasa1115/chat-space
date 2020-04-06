0f0365a5a64c5eae12ce0d434bd3aea5aae6ee22
kaimasa1115 <62590844+kaimasa1115@users.noreply.github.com>
2020-04-06 16:02:24 +0900

    b

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,5 +12,5 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chat-space-masa'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-masa'
+  config.fog_directory  = 'chatspace-app-masa'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspace-app-masa'
